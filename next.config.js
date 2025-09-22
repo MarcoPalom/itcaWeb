96a270d76044b24911085d5639be59ec809010e6
Marco Palomo <166267258+MarcoPalom@users.noreply.github.com>
2025-09-22 02:18:17 -0600

    cambios

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
     domains: [],
   },
-  distDir: 'out',
   async rewrites() {
     return []
   },
